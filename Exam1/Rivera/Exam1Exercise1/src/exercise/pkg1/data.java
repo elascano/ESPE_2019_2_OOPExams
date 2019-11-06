303a7fe3258390d6aa1daddcca9bb93888f11a9b
STALIN RIVERA VEGA <55595831+Stalin0@users.noreply.github.com>
2019-11-06 11:49:35 -0500

    V1.0

---
+++ Exam1/Rivera/Exam1Exercise1/src/exercise/pkg1/data.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise.pkg1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class data {
+    private String litle;
+    private String synopsis;
+    private int year;
+
+    public String getLitle() {
+        return litle;
+    }
+
+    public void setLitle(String litle) {
+        this.litle = litle;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
+}

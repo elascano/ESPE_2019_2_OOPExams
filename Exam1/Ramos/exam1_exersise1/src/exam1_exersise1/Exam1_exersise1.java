f9cefa3a1cc0cf2548147b92f4da6f105abca944
jorgera10 <jorgeramosgc@gmail.com>
2019-11-06 11:56:10 -0500

    exam1Exercise1

---
+++ Exam1/Ramos/exam1_exersise1/src/exam1_exersise1/Exam1_exersise1.java
@@ -0,0 +1,49 @@
+package exam1_exsersie1;
+
+
+public class MovieCenter {
+
+    public static void main(String[] args) {
+     
+        MovieCenter movie = new MovieCenter;
+        
+        private String title;
+        private String synopsis;
+        private int year;
+
+    public String getTitle() {
+        System.out.println("Digite el titulo del libro: ");
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+        
+        
+        
+        
+    }
+        
+        
+        
+    
+    
+

a99948fd4fac468684d86f653485fa5049929206
DiegoPaez2 <diegolexx99@gmail.com>
2019-11-06 11:46:36 -0500

    Exam Exercise1

---
+++ Exam1/PáezDiego/OneExerciseOne/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import oneexerciseone.Movie;
+
+public class Main {
+
+    public static void main(String[] arg) {
+
+        Scanner scn = new Scanner(System.in);
+
+        ArrayList<Movie> list = new ArrayList<Movie>();
+
+        while (true) {
+            System.out.println("Enter the tltle, synopsis, year, genre, country, direcctor ");
+            String aux = scn.nextLine();
+            String[] vector = aux.split(" ");
+            
+            if (aux.equals("")) {
+                break;
+            }
+
+            String title = vector[0];
+            String synopsis = vector[1];
+            int year = Integer.parseInt(vector[2]);
+            String genre = vector[3];
+            String country = vector[4];
+            String director = vector[5];
+
+ 
+            Movie movie = new Movie(title, synopsis, year, genre, country, director);
+
+            list.add(movie);
+
+        }
+
+        System.out.println("\n\nImpresion  ");
+        for (int i = 0; i < list.size(); i++) {
+            Movie aux = list.get(i);
+            System.out.println(aux.getTitle() + " - " + aux.getSynopsis() +" - " + aux.getYear()+" - " + aux.getGenre()+" - " + aux.getCountry()+" - " + aux.getDirector());
+        }
+    }
+}
---
+++ Exam1/PáezDiego/OneExerciseOne/src/oneexerciseone/Director.java
@@ -0,0 +1,47 @@
+package oneexerciseone;
+
+public class Director {
+    private String firstName;
+    private String lastName;
+    private String country;
+    
+    
+    public Director(String firstName, String lastName, String country ){
+        
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        
+        
+    }
+            
+            
+            
+            
+            
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    
+}

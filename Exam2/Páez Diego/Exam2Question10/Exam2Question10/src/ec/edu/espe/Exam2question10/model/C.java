51ee0da8b6b3496569d97516d9e4b38fc4c9bf5b
DarianMichael <56969322+DarianMichael@users.noreply.github.com>
2019-12-11 11:50:17 -0500

    Exam2

---
+++ Exam2/Páez Diego/Exam2Question10/Exam2Question10/src/ec/edu/espe/Exam2question10/model/C.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.Exam2question10.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author Labs-DCCO
+ */
+public class C extends A{
+    
+    private List<E> e; 
+    
+}

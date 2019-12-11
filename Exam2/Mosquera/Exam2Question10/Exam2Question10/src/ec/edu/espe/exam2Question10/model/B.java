673b7c633aca5a11f2350b5227f26b541b33a160
Adrian Mosquera <55595745+Nathaniel2902@users.noreply.github.com>
2019-12-11 12:35:49 -0500

    Exam2Exercises10and11

---
+++ Exam2/Mosquera/Exam2Question10/Exam2Question10/src/ec/edu/espe/exam2Question10/model/B.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.exam2Question10.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Steven
+ */
+public class B extends A {
+    private H hs = new G();
+
+    /**
+     * @return the hs
+     */
+    public H getHs() {
+        return hs;
+    }
+
+    /**
+     * @param hs the hs to set
+     */
+    public void setHs(H hs) {
+        this.hs = hs;
+    }
+
+    @Override
+    public String toString() {
+        return "B{" + "hs=" + hs + '}';
+    }
+
+    public B() {
+    }
+   
+    
+    
+    
+}
---
+++ Exam2/Mosquera/Exam2Question10/Exam2Question10/src/ec/edu/espe/exam2Question10/model/E.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.exam2Question10.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Steven
+ */
+public class E {
+
+    @Override
+    public String toString() {
+        return "E{" + '}';
+    }
+    
+  
+}

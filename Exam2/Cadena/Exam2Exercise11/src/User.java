eebde7540d8294a339ca48f6d2b10ab058ba21f3
pacadena5 <pacadena5@espe.edu.ec>
2019-12-11 12:02:59 -0500

    Cadena

---
+++ Exam2/Cadena/Exam2Exercise11/src/User.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author H304
+ */
+public class User {
+    private String name;
+    private String password;
+    
+}
